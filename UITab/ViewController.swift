4fe5e10f1c93b14d138311a0ae2900c4aecdf549
Vincent Luycx <vincentluycx@macbook-pro-de-vincent.local>
2018-07-22 19:57:23 +0200

    Initial Commit

---
+++ UITab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITab
+//
+//  Created by Vincent Luycx on 22/07/2018.
+//  Copyright © 2018 Vincent Luycx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
